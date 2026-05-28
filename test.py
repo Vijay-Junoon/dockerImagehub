0efe286c197f4c9e2b252669f9f47817ce13bbf9
V Vijay <vijay262005@gmail.com>
2026-05-28 15:54:17 +0530

    Major code refactoring

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from app import app
 
 def test_home():
-  response = app.test_client().get("/")
-
-  assert response.status_code == 200
-  assert response.data == "Hello World"
+    response = app.test_client().get("/")
+    
+    assert response.status_code == 200
+    assert response.get_data(as_text=True) == "Hello World!"
